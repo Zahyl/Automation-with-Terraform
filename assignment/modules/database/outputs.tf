f22dd332a37168cffb2490b391433ebb7b4a8d24
Sahil Solanki <71133664+Zahyl@users.noreply.github.com>
2023-03-15 16:20:08 -0400

    Delete assignment directory

--- assignment/modules/database/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "postgresql_server_name" {
-  description = "The name of the PostgreSQL Server"
-  value       = azurerm_postgresql_server.postgresql_server.name
-}
--- assignment/modules/datadisk/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "linux_datadisk1_ids" {
-  value = azurerm_managed_disk.linux_disk1.*.id
-}
-
-output "windows_datadisk1_id" {
-  value = azurerm_managed_disk.windows_disk1.id
-}
